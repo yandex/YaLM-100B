09187f20241f195fe4089c3f887b81d8c8038dc9
Vasilev Ruslan <53991623+artnitolog@users.noreply.github.com>
2022-06-23 10:29:57 +0300

    fix links

--- download/download.sh
+++ download/download.sh
@@ -3,7 +3,7 @@
 mkdir -p yalm100b_checkpoint/vocab yalm100b_checkpoint/weights
 
 cd yalm100b_checkpoint/vocab
-curl --remote-name-all https://yalm-100b.s3.mds.yandex.net/vocab/voc_100b.sp
+curl --remote-name-all https://yalm-100b.s3.yandex.net/vocab/voc_100b.sp
 
 cd ../weights
-curl --remote-name-all https://yalm-100b.s3.mds.yandex.net/weights/layer_{00,01,[03-82],84}-model_00-model_states.pt
+curl --remote-name-all https://yalm-100b.s3.yandex.net/weights/layer_{00,01,[03-82],84}-model_00-model_states.pt
